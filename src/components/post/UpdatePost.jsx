d470c6b729db4ab4b2d55e74a289fea9330fcb57
kaushalshakya <kshakya101@gmail.com>
2023-09-07 15:38:51 +0545

    Post delete modified

--- src/components/post/UpdatePost.jsx
+++ src/components/post/UpdatePost.jsx
@@ -1,10 +1,62 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
-import { allPosts, getPosts } from '../../slices/postSlice';
+import { useNavigate, useParams } from 'react-router-dom'
+import { 
+    allPosts, 
+    getPosts,
+    resetSuccess,
+    deletePost,
+    postSuccess,
+ } from '../../slices/postSlice';
+import { getUser } from '../../slices/authSlice';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const UpdatePost = () => {
     const dispatch = useDispatch();
+    const user = useSelector(getUser);
+    const navigate = useNavigate()
+
+    const [confirmDelete, setConfirmDelete] = useState(false);
+    const [deleteId, setDeleteId] = useState(null);
+    const success = useSelector(postSuccess);
+
+    console.log('success', success );
+
+    useEffect(() => {
+      if(success) {
+        toast.success(success.message, {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        })
+        setTimeout(() => {
+            dispatch(resetSuccess());
+            navigate('/')
+        }, 2100)
+      }
+    }, [success, dispatch])
+
+    const handleDelete = (id) => {
+      setConfirmDelete(true);
+      setDeleteId(id);
+    }
+
+    const deleteConfirm = () => {
+      dispatch(deletePost(deleteId));
+      setConfirmDelete(false);
+      setDeleteId(null);
+    }
+
+    const cancelDelete = () => {
+      setConfirmDelete(false);
+      setDeleteId(null);
+    }
 
     useEffect(() => {
         dispatch(getPosts());
@@ -16,12 +68,36 @@ const UpdatePost = () => {
     const post = posts.response.filter(post => post.id == id);
     console.log(post);
   return (
-    <div>
-        <h1>Post ID: {post[0].id}</h1>
-        <h1>Post Title: {post[0].post_title}</h1>
-        <h1>Post Content: {post[0].post_content}</h1>
-        <h1>Post Image: {post[0].post_image ? post[0].post_image : 'null'}</h1>
+    <>
+    <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col">
+            {post[0].post_image && <img src={post[0].post_image} className="max-w-[1000px] rounded-lg shadow-2xl" />}
+            <div>
+                <h1 className="text-5xl font-bold">{post[0].post_title}</h1>
+                <p className="py-6">{post[0].post_content}</p>
+                {user && (user.id === post[0].user_id) && <button type = 'button' className="btn btn-primary">Update </button>}
+                {user && (user.id === post[0].user_id) && <button type = 'button' onClick={() => handleDelete(post[0].id)} className="btn btn-error">Delete </button>}
+            </div>
+        </div>
+      <ToastContainer />
     </div>
+    {confirmDelete && ( 
+      <>
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="modal modal-open">
+            <div className="modal-box">
+              <h2 className="text-xl mb-1 font-bold">Confirm Deletion</h2>
+              <p>Are you sure you want to delete this post?</p>
+              <div className="modal-action">
+                <button onClick={deleteConfirm} className="btn btn-primary">Yes, Delete</button>
+                <button onClick={cancelDelete} className="btn btn-ghost">Cancel</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )}
+    </>
   )
 }
 
--- src/components/post/ViewPosts.jsx
+++ src/components/post/ViewPosts.jsx
@@ -4,16 +4,12 @@ import {
     postStatus, 
     postError, 
     getPosts, 
-    deletePost,
     postSuccess,
-    resetSuccess
 } from "../../slices/postSlice";
-import { ToastContainer, toast } from "react-toastify";
-import React, { useState } from 'react'
+import React from 'react'
 import { useEffect } from "react";
 import Loader from "../Loader";
 import { getUser } from "../../slices/authSlice";
-import 'react-toastify/dist/ReactToastify.css';
 import { Link } from "react-router-dom";
 
 const ViewPosts = () => {
@@ -23,48 +19,12 @@ const ViewPosts = () => {
     const error = useSelector(postError);
     const user = useSelector(getUser);
     const success = useSelector(postSuccess);
-    
-    const [confirmDelete, setConfirmDelete] = useState(false);
-    const [deleteId, setDeleteId] = useState(null);
 
     console.log('success', success );
 
     useEffect(() => {
       dispatch(getPosts());
     }, [dispatch]);
-
-    useEffect(() => {
-      if(success) {
-        toast.success(success.message, {
-          position: "top-right",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "dark",
-        })
-        dispatch(resetSuccess());
-      }
-    }, [success, dispatch])
-
-    const handleDelete = (id) => {
-      setConfirmDelete(true);
-      setDeleteId(id);
-    }
-
-    const deleteConfirm = () => {
-      dispatch(deletePost(deleteId));
-      setConfirmDelete(false);
-      setDeleteId(null);
-      dispatch(getPosts());
-    }
-
-    const cancelDelete = () => {
-      setConfirmDelete(false);
-      setDeleteId(null);
-    }
 
     var content;
 
@@ -75,6 +35,7 @@ const ViewPosts = () => {
         const postList = posts.response;
         const orderedList = postList && postList.slice().sort((a,b) => b.post_added.localeCompare(a.post_added));
         content = postList ? orderedList.map(post => (
+          <Link to={`/${post.id}`}>
             <div key={post.id} className="card w-[1000px]  bg-base-300 shadow-xl">
             {post.post_image && <figure><img src={post.post_image} alt="Post image" /></figure>}
             <div className="card-body">
@@ -82,7 +43,6 @@ const ViewPosts = () => {
                 {post.post_title}
               </h2>
               <p>{post.post_content}</p>
-              <Link to = {`/${post.id}`}><button type = 'button' className="btn w-[90px] h-7 ml-[53rem] btn-primary">Update</button></Link>
               {user && user.id === post.user_id && <div className="flex items-end">
                 <button type = 'button' onClick = {() => handleDelete(post.id)} className="btn w-[90px] h-7 ml-[53rem] btn-error">Delete</button>
               </div>}
@@ -91,6 +51,7 @@ const ViewPosts = () => {
               </div>
             </div>
           </div>
+          </Link>
         )) : 
         <div className="hero min-h-screen bg-base-200">
           <div className="hero-content text-center">
@@ -110,24 +71,7 @@ const ViewPosts = () => {
     <>
     <div className="flex flex-col m-4 items-center gap-4 justify-center">
       {content}
-      <ToastContainer />
     </div>
-    {confirmDelete && ( 
-      <>
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="modal modal-open">
-            <div className="modal-box">
-              <h2 className="text-xl mb-1 font-bold">Confirm Deletion</h2>
-              <p>Are you sure you want to delete this post?</p>
-              <div className="modal-action">
-                <button onClick={deleteConfirm} className="btn btn-primary">Yes, Delete</button>
-                <button onClick={cancelDelete} className="btn btn-ghost">Cancel</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    )}
     </>
   )
 }
